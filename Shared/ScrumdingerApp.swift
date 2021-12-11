99eefa297a09b5e1e49ef44ee1f2a03cb977e1f9
Kihoon Lee <kihooncs@gmail.com>
2021-12-10 23:18:59 -0800

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Kihoon Lee on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
